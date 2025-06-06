ad53a13bb023154e697160c847af787fc79e5ae9
vaibhav-pandey4029 <vaibhapandey68@gmail.com>
2025-06-07 00:54:57 +0530

    Dynamic signup API and middleware to parse JSON

--- src/app.js
+++ src/app.js
@@ -3,18 +3,21 @@ const {connectDB} = require("./config/database");
 const {User} = require("./model/user")
 const app = express();
 
+//This is the middleware we are adding to parse the JSON received from API request.body to JS Object so that it can be readed successfully beacuse if we will not add this them req.body will be undefined this middleware will be called each time when any route will get hit by users as we did not provided any path to it.
+app.use(express.json());
+
 app.post("/signup",async (req,res)=>{
     // Create a dummy data object
-    const userObj = {
-        firstName:"Vaibhav",
-        lastName:"Pandey",
-        emailId:"vaibhav@gmail.com",
-        password:"12345"
-    }
-
+    // const userObj = {
+    //     firstName:"Vaibhav",
+    //     lastName:"Pandey",
+    //     emailId:"vaibhav@gmail.com",
+    //     password:"12345"
+    // }
+    
+    // create a new instance of user model using the object now it is dynamic
+    const newUser = new User(req.body);
     try{
-        // create a new instance of user model using the object
-        const newUser = new User(userObj);
         await newUser.save();
         res.send("User added successfully")
     }catch(error){
