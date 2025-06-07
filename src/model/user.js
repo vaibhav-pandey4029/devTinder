581d55b1676a9735c78cbe2b71980a13d09df1d8
vaibhav-pandey4029 <vaibhapandey68@gmail.com>
2025-06-07 17:11:49 +0530

    Added validation to check valid email and strong password

--- src/model/user.js
+++ src/model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -15,10 +16,20 @@ const userSchema = new mongoose.Schema({
         required:true,
         unique:true,
         trim:true,
-        lowercase:true
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is not Valid");
+            }
+        }
     },
     password:{
-        type:String
+        type:String,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password");
+            }
+        }
     },
     age:{
         type:Number,
